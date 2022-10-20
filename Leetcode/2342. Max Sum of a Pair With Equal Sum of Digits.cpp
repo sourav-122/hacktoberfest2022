9fb95d22639547256365588804b2117641a7cb30
SubodhSingh5436 <93370103+SubodhSingh5436@users.noreply.github.com>
2022-10-20 11:51:09 +0530

    Create 2342. Max Sum of a Pair With Equal Sum of Digits.cpp

---
+++ Leetcode/2342. Max Sum of a Pair With Equal Sum of Digits.cpp
@@ -0,0 +1,34 @@
+// You are given a 0-indexed array nums consisting of positive integers. You can choose two indices i and j, such that i != j, and the sum of digits of the number nums[i] is equal to that of nums[j].
+
+// Return the maximum value of nums[i] + nums[j] that you can obtain over all possible indices i and j that satisfy the conditions.
+// Example 1:
+
+// Input: nums = [18,43,36,13,7]
+// Output: 54
+// Explanation: The pairs (i, j) that satisfy the conditions are:
+// - (0, 2), both numbers have a sum of digits equal to 9, and their sum is 18 + 36 = 54.
+// - (1, 4), both numbers have a sum of digits equal to 7, and their sum is 43 + 7 = 50.
+// So the maximum sum that we can obtain is 54.
+// Example 2:
+
+// Input: nums = [10,12,19,14]
+// Output: -1
+// Explanation: There are no two numbers that satisfy the conditions, so we return -1.
+
+class Solution {
+public:
+    int maximumSum(vector<int>& nums) {
+        int ans=-1, sz=nums.size();
+        unordered_map<int,int>mp;
+        for(auto & i:nums){
+            string s=to_string(i);
+            int sum=0;
+            for(auto & ch:s)
+                sum+=(ch-'0');
+            if(mp.count(sum))
+                ans=max(ans,i+mp[sum]);
+            mp[sum]=max(i,mp[sum]);
+        }
+        return ans;
+    }
+};
